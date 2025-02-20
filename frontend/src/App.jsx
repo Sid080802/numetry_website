846a6412183c5966168c8d8795789e7840540c58
Siddharth Godambe <siddarthgodambe123@gmail.com>
2025-02-20 09:58:11 +0530

    hire devloper updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,10 @@ import Home from './pages/Home';
 // import AboutUs from './pages/AboutUs';
 // import CareerPage from './pages/CareerPage';
 // import ContactUs from './pages/ContactUs';
-// import HireDeveloper from './pages/HireDeveloper';
+import HireDeveloper from './pages/HireDeveloper';
 // import TrainingCourses from './pages/TrainingCourses';
 // import Footer from './components/Footer';
+
 
 import './App.css';
 
@@ -21,11 +22,12 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/hire" element={<HireDeveloper />} />
           {/* <Route path="/navbar" element={<Navbar />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/careerpage" element={<CareerPage />} />
           <Route path="/contactus" element={<ContactUs />} />
-          <Route path="/hiredeveloper" element={<HireDeveloper />} />
+
           <Route path="/trainingcourses" element={<TrainingCourses />} />
           <Route path="/footer" element={<Footer />} /> */}
         </Routes>
